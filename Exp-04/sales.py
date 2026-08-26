29c9b5b20c2561ef570f2351d98fe346e8a2b4da
roopa <roopajallu814@gmail.com>
2026-08-26 13:43:05 +0530

    Organize Experiments 1 to 13

---
+++ Exp-04/sales.py
@@ -0,0 +1,4 @@
+sales = [10000, 15000, 12000, 18000, 20000, 22000, 25000, 28000, 30000, 32000, 35000, 40000]
+total_sales = sum(sales)
+print("Total sales:", total_sales)
+print("Average:", total_sales / len(sales))
