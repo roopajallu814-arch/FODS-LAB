303d126dc1fde073b06725b33ba50ddd0247bec2
roopa <roopajallu814@gmail.com>
2026-08-26 13:44:35 +0530

    Organize Experiments 1 to 13

---
+++ Exp-05/fuel_efficiency.py
@@ -0,0 +1,4 @@
+fuel_efficiency = [25.5, 28.3, 22.1, 30.5, 26.8, 24.5, 29.1]
+average = sum(fuel_efficiency) / len(fuel_efficiency)
+print("Fuel efficiency values:", fuel_efficiency)
+print("Average:", average)
