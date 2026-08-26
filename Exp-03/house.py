bf20ba9a35f4e369c6967269e7a5d98ebfb92263
roopa <roopajallu814@gmail.com>
2026-08-26 13:42:37 +0530

    Organize Experiments 1 to 13

---
+++ Exp-03/house.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+house_prices = np.array([250000, 300000, 350000, 400000, 450000])
+print("House prices:", house_prices)
+print("Average:", np.mean(house_prices))
+print("Max:", np.max(house_prices))
